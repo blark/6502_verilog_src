a95ad7af1b6674c867e1930c2356cd5806e0b447
Mark Baseggio <blark@pwnp.al>
2013-11-04 15:55:19 -0500

    added some new files

---
+++ clk_switch_debounce.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:13:51 11/03/2013 
+// Design Name: 
+// Module Name:    clk_switch_debounce 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module clk_switch_debounce(
+    );
+
+
+endmodule
---
+++ clock_div.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// 
+// This module generates a 1Mhz clock for the 6502 MPU. 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+module mpu_clock_div(
+    input clk,
+	 input rst,
+	 input clk_en,
+	 input step_press,
+	 output reg mpu_clk
+	 );
+
+localparam PERIOD = 24; // 1Mhz with 50% duty cycle.
+
+reg [4:0] countvalue = 5'b0; 
+
+always @(posedge clk) begin
+  if(rst) begin
+    countvalue <= 5'b0;
+    mpu_clk <= 1'b0;
+    end
+  else begin
+    if(countvalue == PERIOD) begin
+      countvalue <= 5'b0;	// reset counter to 00000
+      mpu_clk <= ~mpu_clk; // toggle mpu clock signal
+      end
+	 if (clk_en && mpu_clk) begin
+		//clock stops high when clk_en is true. 
+	   end
+    else countvalue <= countvalue + 5'b1;
+    end
+end
+  
+endmodule
