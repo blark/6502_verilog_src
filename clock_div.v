43a65782a5993278d728c177260cdcbd12e80dc3
Mark Baseggio <blark@pwnp.al>
2013-11-04 16:20:39 -0500

    fixed stuff

--- clock_div.v
+++ clock_div.v
@@ -1,36 +1,37 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // 
-// This module generates a 1Mhz clock for the 6502 MPU. 
+// This module generates a 1Mhz clock for the 6502 MPU (from a 50Mhz clock).
 //
 //////////////////////////////////////////////////////////////////////////////////
 
 module mpu_clock_div(
 	input clk,
 	input rst,
-	input clk_en,
-	input step_press,
+	input clk_en, // debounced clock_enable switch
+	input step_press, // debounced step button
 	output reg mpu_clk
 	);
 
-localparam PERIOD = 24; // 1Mhz with 50% duty cycle.
+// 1Mhz with 50% duty cycle. 
+localparam PERIOD = 24; 
 
-reg [4:0] countvalue = 5'b0; 
+reg [4:0] clk_count = 5'b0; 
 
 always @(posedge clk) begin
   if(rst) begin
-    countvalue <= 5'b0;
+    clk_count <= 5'b0;
     mpu_clk <= 1'b0;
     end
   else begin
-    if(countvalue == PERIOD) begin
-      countvalue <= 5'b0;	// reset counter to 00000
-      mpu_clk <= ~mpu_clk; // toggle mpu clock signal
+    if(clk_count == PERIOD) begin
+      clk_count <= 5'b0;	// reset counter
+      mpu_clk <= ~mpu_clk; // toggle mpu clock
       end
-    if (clk_en && mpu_clk) begin
+    //if (clk_en && mpu_clk) begin
       //clock stops high when clk_en is true. 
-      end
-    else countvalue <= countvalue + 5'b1;
+      //end
+    else clk_count <= clk_count + 5'b1;
     end
 end
   
