63af06500cf171ccd8ee48be9afeec34053d6053
Mark Baseggio <blark@pwnp.al>
2013-11-04 16:06:39 -0500

    fixed some spacing crap

--- clock_div.v
+++ clock_div.v
@@ -6,12 +6,12 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module mpu_clock_div(
-    input clk,
-	 input rst,
-	 input clk_en,
-	 input step_press,
-	 output reg mpu_clk
-	 );
+	input clk,
+	input rst,
+	input clk_en,
+	input step_press,
+	output reg mpu_clk
+	);
 
 localparam PERIOD = 24; // 1Mhz with 50% duty cycle.
 
@@ -27,9 +27,9 @@ always @(posedge clk) begin
       countvalue <= 5'b0;	// reset counter to 00000
       mpu_clk <= ~mpu_clk; // toggle mpu clock signal
       end
-	 if (clk_en && mpu_clk) begin
-		//clock stops high when clk_en is true. 
-	   end
+    if (clk_en && mpu_clk) begin
+      //clock stops high when clk_en is true. 
+      end
     else countvalue <= countvalue + 5'b1;
     end
 end
